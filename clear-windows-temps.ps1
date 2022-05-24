3152db976ce5bc98bf4e5a9c1ce153f9fa8db197
Shaul Mo <95495331+shaulmo@users.noreply.github.com>
2022-05-24 14:46:10 +0300

    Update clear-windows-temps.ps1

--- clear-windows-temps.ps1
+++ clear-windows-temps.ps1
@@ -1,3 +1,4 @@
 # Clearing Windows Temp Folders including users %LOCALAPPDATA% Local temp folders
-$tempDirs = @(“C:\Windows\Temp\*”, “C:\Windows\Prefetch\*”, “C:\Users\*\Appdata\Local\Temp\*”)
-Remove-Item $tempDirs -Force -Recurse -ErrorAction SilentlyContinue
+# Make sure to run with ISE | Ignores errors with files already in use **
+$tmpDirs = @(“C:\Windows\Temp\*”, “C:\Windows\Prefetch\*”, “C:\Documents and Settings\*\Local Settings\temp\*”, “C:\Users\*\Appdata\Local\Temp\*”)
+Remove-Item $tmpDirs -Force -Recurse -ErrorAction SilentlyContinue
