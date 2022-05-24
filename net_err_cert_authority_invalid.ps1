2c8437430a1c4a142b59c7d4a04865f4c1e404da
Shaul Mo <95495331+shaulmo@users.noreply.github.com>
2022-05-24 21:56:47 +0300

    Update net_err_cert_authority_invalid.ps1

--- net_err_cert_authority_invalid.ps1
+++ net_err_cert_authority_invalid.ps1
@@ -1,5 +1,5 @@
 # Make sure to run with ISE 
-cd C:\
+Set-Location C:\
 certutil.exe -generateSSTFromWU roots.sst
 $sstWU = ( Get-ChildItem -Path C:\roots.sst )
 $sstWU | Import-Certificate -CertStoreLocation Cert:\LocalMachine\Root
