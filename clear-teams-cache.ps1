507cc43a53040d41e2ffa378b7102d3dc4e03bb0
Shaul Mo <95495331+shaulmo@users.noreply.github.com>
2022-05-25 04:25:48 +0300

    Update clear-teams-cache.ps1

--- clear-teams-cache.ps1
+++ clear-teams-cache.ps1
@@ -1,5 +1,4 @@
-# Sometimes updates and cache files causes issues with the Teams apps in an enterprise environment.
-
+## Sometimes updates and cache files causes issues with the Teams App running in an enterprise environment.
 # Stopping Teams Process, Doesn't show Error if Teams is already not running.
 Stop-Process -Name Teams -ErrorAction SilentlyContinue
 # Clearing Known Cache folders for all users in the Windows PC Host.
